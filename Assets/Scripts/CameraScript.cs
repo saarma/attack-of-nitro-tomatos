e8522f3289337eda99b09b804e6b5e311a59f6ed
Miika Kontio <miikako89@gmail.com>
2024-01-27 14:43:50 +0200

    camera stays on position 1 or position 2 after pressing V

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -24,20 +24,22 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.V)) {
-            isCameraInOriginalPosition = false;
-        } else {
-            isCameraInOriginalPosition = true;
+        var changeCameraPosition = false;
+        if(Input.GetKeyDown(KeyCode.V)) {
+            isCameraInOriginalPosition = !isCameraInOriginalPosition;
+            changeCameraPosition = true;
         }
 
-        if(isCameraInOriginalPosition) {
-            this.camera.transform.position = cameraPosition1Target.transform.position;
-            float y = 0f;
-            this.camera.transform.rotation = Quaternion.AngleAxis(y, Vector3.up);
-        } else {
-            this.camera.transform.position = cameraPosition2Target.transform.position;
-            float y = 20f;
-            this.camera.transform.rotation = Quaternion.AngleAxis(y, Vector3.up);
+        if(changeCameraPosition) {
+            if(isCameraInOriginalPosition) {
+                this.camera.transform.position = cameraPosition1Target.transform.position;
+                float y = 0f;
+                this.camera.transform.rotation = Quaternion.AngleAxis(y, Vector3.up);
+            } else {
+                this.camera.transform.position = cameraPosition2Target.transform.position;
+                float y = 20f;
+                this.camera.transform.rotation = Quaternion.AngleAxis(y, Vector3.up);
+            }
         }
     }
 }
