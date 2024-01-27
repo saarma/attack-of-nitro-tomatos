5bfb054e63d4379f05d63211e1432e11680667cc
Miika Kontio <miikako89@gmail.com>
2024-01-27 12:47:53 +0200

    tomatoes made nice (#14)

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,7 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject tomato;
-    float spawnTimer = 1f;
+    float spawnTimer = 3f;
     float timer = 0f;
     int waveNumber = 0;
 
