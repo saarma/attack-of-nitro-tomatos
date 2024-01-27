6f7100f00d71dee42e8ba79149f82e61beee3782
erjaman1 <erjamanninen1@kamk.fi>
2024-01-27 22:51:12 +0200

    psoundeffects

---
+++ Assets/RandomSound.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSound : MonoBehaviour
+{
+    [SerializeField] AudioClip[] spawnSounds;
+    [SerializeField] AudioClip[] deathSounds;
+    
+    AudioSource myAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      myAudioSource = GetComponent<AudioSource>();
+      SpawnSound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy enemy = GetComponent<Enemy>();
+
+        if (enemy.IsDead)
+        { 
+            DeathSound();
+        }
+
+    }
+
+    void SpawnSound()
+    {
+        AudioClip clip = spawnSounds[UnityEngine.Random.Range(0, spawnSounds.Length-1)];
+        myAudioSource.PlayOneShot(clip);
+    }
+
+    void DeathSound()
+    {
+        AudioClip clip = deathSounds[UnityEngine.Random.Range(0, deathSounds.Length-1)];
+        myAudioSource.PlayOneShot(clip);
+    }
+}
